eea366d94176aaea0a00a68ff2a171da343095d4
Arko Kundu <kunduarko@gmail.com>
2021-04-11 11:19:21 -0400

    added move5

---
+++ moves/move4.py
@@ -0,0 +1,21 @@
+from lx16a import *
+from math import sin, cos
+import time
+
+LX16A.initialize('/dev/ttyUSB0')
+
+
+left_knee = LX16A(8)
+left_thigh = LX16A(7)
+left_hip = LX16A(6)
+right_knee = LX16A(3)
+right_thigh = LX16A(2)
+right_hip = LX16A(1)
+right_hand = LX16A(4)
+left_hand = LX16A(5)
+
+t = 0
+while True:
+    right_hand.moveTimeWrite(238 + sin(t)*10)
+    #left_hand.moveTimeWrite(160+sin(t)*50)
+    t += 0.005
---
+++ moves/move5.py
@@ -0,0 +1,49 @@
+from lx16a import *
+from math import sin, cos
+import time
+
+LX16A.initialize('/dev/ttyUSB0')
+
+
+left_knee = LX16A(8)
+left_thigh = LX16A(7)
+left_hip = LX16A(6)
+right_knee = LX16A(3)
+right_thigh = LX16A(2)
+right_hip = LX16A(1)
+right_hand = LX16A(4)
+left_hand = LX16A(5)
+
+#print("Left knee : " , left_knee.getPhysicalPos())
+#print("Left thigh : " , left_thigh.getPhysicalPos())
+#print("Right Knee : " , right_knee.getPhysicalPos())
+#print("Right Thigh : " , right_thigh.getPhysicalPos())
+print(" Left Hand : " , left_hand.getPhysicalPos())
+#print("Left Hip : " , left_hip.getPhysicalPos())
+print("Right hand : " , right_hand.getPhysicalPos())
+#print("Right hip : " , right_hip.getPhysicalPos())
+
+
+
+left_hand.moveTimeWrite(239,time=1000)
+time.sleep(1)
+right_hand.moveTimeWrite(180,time=1000)
+
+time.sleep(2)
+
+right_hand.moveTimeWrite(240,time=1000)
+time.sleep(1)
+left_hand.moveTimeWrite(200,time=1000)
+t = 0
+
+##while True:
+ #   right_hand.moveTimeWriteRel(sin(t)*10)
+  #  left_hand.moveTimeWriteRel(sin(t)*10)
+  #  t += 0.005
+
+#left_leg.moveTimeWriteRel(10, 3000)
+#time.sleep(1)
+#right_leg.moveTimeWriteRel(10, 3000)
+
+#right_leg.moveTimeWriteRel(10,2000)
+#left_kne.moveTimeWriteRel(10,2000)
