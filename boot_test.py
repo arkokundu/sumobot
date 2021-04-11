6e3e18a71198833267b8e0fcfcd8763d9380a5fd
Arko Kundu <kunduarko@gmail.com>
2021-04-10 23:27:16 -0400

    added moves directory

--- boot_test.py
+++ boot_test.py
@@ -10,19 +10,19 @@ right_knee = LX16A(2)
 
 pos1 = left_leg.getPhysicalPos()
 print("The left leg motor physical position is {} degrees".format(pos1))
-print("The left leg motor temperature (in deg C) is {}".format(left_leg.tempRead()))
+#print("The left leg motor temperature (in deg C) is {}".format(left_leg.tempRead()))
 
-pos2 = left_knee.getPhysicalPos()
-print("The left knee motor physical position is {} degrees".format(pos2))
-print("The left knee motor temperature (in deg C) is {}".format(left_knee.tempRead()))
+#pos2 = left_knee.getPhysicalPos()
+#print("The left knee motor physical position is {} degrees".format(pos2))
+#print("The left knee motor temperature (in deg C) is {}".format(left_knee.tempRead()))
 
-pos3 = right_leg.getPhysicalPos() 
-print("The right leg motor physical position is {} degrees".format(pos3))
-print("The right leg motor temperature (in deg C) is {}".format(right_leg.tempRead()))
+#pos3 = right_leg.getPhysicalPos() 
+#print("The right leg motor physical position is {} degrees".format(pos3))
+#print("The right leg motor temperature (in deg C) is {}".format(right_leg.tempRead()))
 
-pos4 = right_knee.getPhysicalPos()
-print("The right knee motor physical position is {} degrees".format(pos4))
-print("The right knee motor temperature (in deg C) is {}".format(right_knee.tempRead()))
+#pos4 = right_knee.getPhysicalPos()
+#print("The right knee motor physical position is {} degrees".format(pos4))
+#print("The right knee motor temperature (in deg C) is {}".format(right_knee.tempRead()))
 
 
 
